816ebef8e3b944efae48b4057e55c5877b71c164
Sumit Maharjan <sumitjan2@gmail.com>
2023-07-04 21:19:53 +1000

    commit for model and forms test case

--- locallibrary/catalog/tests/test_models.py
+++ locallibrary/catalog/tests/test_models.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
+from catalog.models import Author
+
 # Create your tests here.
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Author.objects.create(first_name='Big', last_name='Bob')
+
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label, 'first name') # this provides the result as well if FALSE
+        #self.assertTrue(field_label == 'first nam') # this should not be used as it only provide TRUE/FALSE
+
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEqual(field_label, 'Died')
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id=1)
+        expected_object_name = f'{author.last_name}, {author.first_name}'
+        self.assertEqual(str(author), expected_object_name)
+
+    def test_get_asolute_url(self):
+        author = Author.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
